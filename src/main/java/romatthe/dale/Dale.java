f8890b88b1c0e776d8c09c7e207b146482d48270
romatthe <robin.mattheussen@gmail.com>
2017-01-16 12:52:35 +0100

    Small refactoring

--- src/main/java/romatthe/dale/Dale.java
+++ src/main/java/romatthe/dale/Dale.java
@@ -3,9 +3,11 @@ package romatthe.dale;
 import romatthe.dale.cpu.Cpu;
 
 import java.io.IOException;
+import java.nio.file.Path;
 
 public class Dale {
     public static void main(String[] args) throws IOException {
+        Path path = java.nio.file.FileSystems.getDefault().getPath("C:/temp");
         Cpu cpu = new Cpu();
     }
 }
